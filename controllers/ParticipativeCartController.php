714aa097d9566c46d43695c5d0e7f9c8471a6746
Franck Dupont <kyfr59@gmail.com>
2018-05-15 09:14:50 +0200

    Redirect non-identified user on specific actions

--- controllers/ParticipativeCartController.php
+++ controllers/ParticipativeCartController.php
@@ -434,6 +434,9 @@ class ParticipativeCart_ParticipativeCartController extends Omeka_Controller_Abs
      */
     public function viewItemAction() {
 
+        // Redirect user to login page if it isn't identify
+        if (!current_user()) $this->_helper->redirector->goToUrl('users/login');
+
         // Check cart ID
         if (!($cart_id = $this->getParam('cart-id'))) {
             throw new Exception("The cart ID is required");
@@ -614,6 +617,9 @@ class ParticipativeCart_ParticipativeCartController extends Omeka_Controller_Abs
      */
     public function switchStatusAction() {
 
+        // Redirect user to login page if it isn't identify
+        if (!current_user()) $this->_helper->redirector->goToUrl('users/login');
+        
         // Disable view rendering
         $this->_helper->viewRenderer->setNoRender(true);
 
